6e0cafbc173139af50bb7be875fa4abc23dc3b2a
Zaninello <camilyzaninello20@gmail.com>
2025-10-20 18:54:07 -0300

    Adding the Program folder to C# code.

---
+++ BromieBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BromieBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando BromieBot");
+        }
+    }
+}
