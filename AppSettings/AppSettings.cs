6ef8a02f22ad4484c1bbb63e281e515fca981010
Taylor S <96550502+taylors42@users.noreply.github.com>
2026-02-03 21:41:27 -0300

    clean

--- AppSettings/AppSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AppSettings;
-
-public static class AppSettings
-{
-    public static string DbConn { get; set; } = string.Empty;
-    public static string TgConn { get; set; } = string.Empty;
-}
