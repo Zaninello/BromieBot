3e9aecf00b294f5234cb9bda7c1a6b281358d6f5
camily <camilyzaninello20@gmail.com>
2026-02-10 16:28:54 -0300

    Adição da função AdicionarUsuario do projeto API

--- API/RepositoryApi/RepositoryUserApi.cs
+++ API/RepositoryApi/RepositoryUserApi.cs
@@ -1,12 +1,19 @@
 ﻿using Models;
-using DataBase; 
+using DataBase;
+using Microsoft.EntityFrameworkCore;
 
 namespace BromieBot.API;
 
 public class RepositoryUserApi(TelegramBotContext context)
 {
-    public bool VerificarUsuario(long chatId)
+    public async Task<bool> VerificarUsuario(long chatId)
     {
-        return context.Users.Any(x => x.ChatId == chatId);
+        return await context.Users.AnyAsync(x => x.ChatId == chatId);
+    }
+
+    public async Task AdicionarUsuario(User usuario)
+    {
+        await context.AddAsync(usuario);
+        await context.SaveChangesAsync();
     }
 }
