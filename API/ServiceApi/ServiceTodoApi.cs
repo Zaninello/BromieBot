39f8e7291952863227c3be42a3b83c35326b59cb
camily <camilyzaninello20@gmail.com>
2026-02-27 12:51:19 -0300

    Criacao da funcao AddTodo no arquivo ServiceTodoApi

--- API/ServiceApi/ServiceTodoApi.cs
+++ API/ServiceApi/ServiceTodoApi.cs
@@ -1,6 +1,27 @@
-﻿namespace API;
+﻿using Models;
+
+namespace API;
 
 public class ServiceTodoApi
 {
+    ServiceTodoApi(RepositoryTodoApi repo)
+    {
+        _repo = repo;
+    }
 
+    private RepositoryTodoApi _repo;
+
+    public async Task<bool> AddTodo(Todo todo)
+    {
+        try
+        {
+            await _repo.AddTodo(todo);
+                return true;
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex);
+                return false;
+        }
+    }
 }
