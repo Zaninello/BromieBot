37e0a67f1fd4aa5e0af644024abd55c6446db641
camily <camilyzaninello20@gmail.com>
2026-02-27 12:55:22 -0300

    Criacao do controller AddTodo no arquivo TodoController.

--- API/Controllers/TodoController.cs
+++ API/Controllers/TodoController.cs
@@ -1,3 +1,4 @@
+using API;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Models;
@@ -6,12 +7,18 @@ namespace BromieBot.API
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TodoController : ControllerBase
+    public class TodoController(ServiceTodoApi service) : ControllerBase
     {
         [HttpPost]
         public async Task<ActionResult> AddTodo([FromBody]Todo todo)
         {
-            return default;
+            var result = await service.AddTodo(todo);
+            
+            if(result == true)
+            {
+                return Ok();
+            }
+            return BadRequest();
         }
     }
 }
