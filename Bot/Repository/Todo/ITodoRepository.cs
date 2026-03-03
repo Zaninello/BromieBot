620c63097c25dbcb22d6f17404734d9075cea60b
camily <camilyzaninello20@gmail.com>
2026-03-03 19:57:12 -0300

    Adicao de retorno Bool na funcao AddTodo na interface ITodoRepository do projeto Bot.

--- Bot/Repository/Todo/ITodoRepository.cs
+++ Bot/Repository/Todo/ITodoRepository.cs
@@ -4,7 +4,7 @@ public interface ITodoRepository
 {
     Task<Models.Todo?> SearchTodoByName(long chatId, string nameTodo);
     Task<IEnumerable<Models.Todo>> GetAllTodos(long chatId);
-    Task AddTodo(Models.Todo todo);
+    Task<bool> AddTodo(Models.Todo todo);
     Task RemoveTodo(Models.Todo todo);
     Task EditTodo(Models.Todo todo, string newDescription);
     Task CompleteTodo(Models.Todo todo);
