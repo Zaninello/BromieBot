5a64a89423f0d406e4edf7625c8775dbb052574b
camily <camilyzaninello20@gmail.com>
2026-03-03 20:00:33 -0300

    Implementacao da funcao AddTodo no TodoRepository do projeto Bot.

--- Bot/Repository/Todo/TodoRepository.cs
+++ Bot/Repository/Todo/TodoRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace Bot.Repository;
+﻿using System.Text.Json;
+
+namespace Bot.Repository;
 
 public class TodoRepository : ITodoRepository
 {
+    private HttpClient _client = new HttpClient();
+
+    string url = "http://localhost:5155/api/Todo";
+    
     public async Task<Models.Todo?> SearchTodoByName(long chatId, string nameTodo)
     {
         throw new NotImplementedException();
@@ -12,9 +18,26 @@ public class TodoRepository : ITodoRepository
         throw new NotImplementedException();
     }
 
-    public async Task AddTodo(Models.Todo todo)
+    public async Task<bool> AddTodo(Models.Todo todo)
     {
-        throw new NotImplementedException();
+        var json = JsonSerializer.Serialize(todo);
+
+        var todoJson = new StringContent(
+            json,
+            Encoding.UTF8,
+            "application/json"
+        );
+
+        try
+        {
+            var result = await _client.PostAsync(url, todoJson);
+                return result.IsSuccessStatusCode;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("erro no request para envio do Todo para a Api: " + ex.Message);
+            return false;
+        }
     }
 
     public async Task RemoveTodo(Models.Todo todo)
