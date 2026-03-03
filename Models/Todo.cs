569fb12485577645748058c71724e3a2fb8e9e5e
camily <camilyzaninello20@gmail.com>
2026-03-03 12:45:57 -0300

    Remoção do atributo User user no arquivo Models/Todo.cs, devido a vinculacao de Todo com usuario ser feita apenas pelo atributo chatId.

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -7,7 +7,6 @@ public class Todo
     [Key]
     public int Id { get; set; }
     public long UserId { get; set; }
-    public User User { get; set; }
     public string? Header { get; set; }
     public string? Description { get; set; }
     public string Status { get; set; } = "pending";
