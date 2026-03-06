804d529037a3d4b75205f31d368c1c97ea1aaea8
camily <camilyzaninello20@gmail.com>
2026-03-06 12:31:57 -0300

    Ajuste de retorno e parametros da Interface ITodoRepositorydevido a logica.

--- Bot/Repository/Todo/ITodoRepository.cs
+++ Bot/Repository/Todo/ITodoRepository.cs
@@ -5,7 +5,7 @@ public interface ITodoRepository
     Task<Models.Todo?> SearchTodoByName(long chatId, string nameTodo);
     Task<IEnumerable<Models.Todo>> GetAllTodos(long chatId);
     Task<bool> AddTodo(Models.Todo todo);
-    Task RemoveTodo(Models.Todo todo);
+    Task<bool> RemoveTodo(long chatId, string nameTodo);
     Task EditTodo(Models.Todo todo, string newDescription);
     Task CompleteTodo(Models.Todo todo);
 }
